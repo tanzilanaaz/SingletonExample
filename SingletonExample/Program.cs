6bb7f60f61bb71f2cc3a8ffc4859715d3e35807f
tanzilanaaz <tanzilanaaz0786@gmail.com>
2024-02-07 17:06:33 +0530

    Add project files.

---
+++ SingletonExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            logging logger1 = logging.Instance;
+            logger1.LogMessage("This message is from logger1");
+
+            logging logger2 = logging.Instance;
+            logger2.LogMessage("This meesage is from logger2");
+
+            Console.WriteLine($"logger1 and logger2 refer to the same instance. Hence: {logger1 == logger2}");
+        }
+    }
+}
---
+++ SingletonExample/logging.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonExample
+{
+    public sealed class logging
+    {
+        private static logging instance;
+        private static readonly object _lock = new object();
+        private logging() {}
+        public static logging Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (_lock)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new logging();
+                        }
+                    }
+                }
+                return instance;
+            }
+        }
+        public void LogMessage(string message)
+        {
+            Console.WriteLine($"[LOG] {DateTime.Now}: {message}");
+        }
+    }
+}
